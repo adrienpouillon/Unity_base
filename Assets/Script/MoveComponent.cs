b32919fdfd44887f1e0c411d9b37c557fa1d4a70
adrienpouillon <apouillon@gaming.tech>
2026-09-24 18:37:54 +0200

    Add asset and create scene

---
+++ Assets/Script/MoveComponent.cs
@@ -0,0 +1,96 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveComponent : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] bool _useForward;
+    [SerializeField] float _speed;
+    [SerializeField] float _jumpForce;
+
+    [Header("Component required")]
+    [SerializeField] Rigidbody _rb;
+    [SerializeField] InputActionReference _moveInput;
+    [SerializeField] InputActionReference _jumpInput;
+    [SerializeField] GameObject _foodPosition;
+    [SerializeField] Camera _camera;
+
+    bool _isGrounded;
+    Vector3 _direction;
+    
+
+    void Reset()
+    {
+        _jumpForce = 1000;
+        _useForward = false;
+        _speed = 1;
+        _rb = GetComponent<Rigidbody>();
+        _moveInput = GetComponent<InputActionReference>();
+        _camera = GetComponent<Camera>();
+    }
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+        _isGrounded = false;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _moveInput.action.started += UpdateMoveInput;
+        _moveInput.action.canceled += EndMoveInput;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_isGrounded = Physics.Raycast(_foodPosition.transform.position, Vector3.down, 1f);// , 10, QueryTriggerInteraction.Collide);
+        if(_jumpInput.action.WasPressedThisFrame())// && _isGrounded)
+        {
+            _rb.AddForce(Vector3.up * _jumpForce);
+        }
+
+        Vector3 cameraForward = _camera.transform.forward;
+        cameraForward.y = 0f;
+        var cameraRight = _camera.transform.right;
+
+        if (_useForward)
+        {
+            transform.Translate(transform.forward * (Time.deltaTime * _speed));
+        }
+        else
+        {
+            var dir = (cameraForward * _direction.z) + (cameraRight * _direction.x);
+            transform.Translate(dir * (Time.deltaTime * _speed));
+        }
+
+        // Rotation du personnage
+        transform.forward = cameraForward;
+    }
+
+    /*void FixedUpdate()
+    {
+        _rb.AddForce(10, 0, 0);
+    }*/
+
+    void OnDestroy()
+    {
+        _moveInput.action.started -= UpdateMoveInput;
+        _moveInput.action.canceled -= EndMoveInput;
+    }
+
+    void UpdateMoveInput(InputAction.CallbackContext obj)
+    {
+        var inputDirection = obj.ReadValue<Vector2>();
+        _direction = new Vector3(inputDirection.x, 0f, inputDirection.y);
+    }
+
+    void EndMoveInput(InputAction.CallbackContext obj)
+    {
+        _direction = Vector3.zero;
+    }
+
+}
+
